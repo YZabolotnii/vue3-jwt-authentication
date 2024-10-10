9df1ae2433edb310ca11c1d0a2e4cb6a7e853888
ZabolotniiYV <yzabolotnii@ukr.net>
2024-10-10 12:06:52 +0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import './style.css'
+import { initializeApp } from "firebase/app";
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4VOld6sJez388qyIyu65V_i7hP5mVpws",
+  authDomain: "vue-jwt-ce647.firebaseapp.com",
+  projectId: "vue-jwt-ce647",
+  storageBucket: "vue-jwt-ce647.appspot.com",
+  messagingSenderId: "432088282251",
+  appId: "1:432088282251:web:c0cdda46837c06b6c4d08e"
+};
+
+initializeApp(firebaseConfig)
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
